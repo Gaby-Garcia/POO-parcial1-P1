23a61c914302850bda6e2f20cfa9619c49ee9c0f
Ana Gabriela Garcia Rodriguez <garciagaby839@gmail.com>
2021-03-05 20:06:38 -0600

    PROYECTOS TERMINADOS

--- src/Practica1/MetodoMain1.java
+++ src/Practica1/MetodoMain1.java
@@ -5,6 +5,7 @@ public class MetodoMain1 {
     public static void main(String[] args){
      Scanner sc = new Scanner(System.in);
      int numfilas;
+        System.out.println("PIRAMIDE ALINEADA A LA IZQUIERDA ");
         System.out.print("¿Cuantas filas desea tener en la piramide? ");
         numfilas= sc.nextInt();
         
--- src/Practica3/MetodoMain3.java
+++ src/Practica3/MetodoMain3.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Practica3;
-
-/**
- *
- * @author Garcia
- */
+import java.util.Scanner;
 public class MetodoMain3 {
+   
+    public static void main (String[] args){
+    Scanner gaby = new Scanner(System.in);
+    int nnum[];
+    int n;
+    int alinear;
     
+        System.out.println(" INGRESAR NUMEROS Y ORDENAR DE MENOR A MAYOR ");
+        System.out.print("¿Cuantos numeros desea ingresar?: ");
+            n=gaby.nextInt();
+             nnum = new int[n];
+             
+             //Se pide al usuario ingresar los numeros 
+             for (int i = 0; i < n; i++) {
+             System.out.print("Ingrese un numero: ");
+             nnum[i]=gaby.nextInt();
+        }
+             //proceso de ordenamiento conocido como el metodo de la burbuja
+                for (int i = 0; i < (n-1); i++) {
+                    for (int j = 0; j < (n-1); j++) {
+                         if (nnum[j]>nnum[j+1]) {
+                               alinear = nnum[j];
+                                    nnum[j]= nnum[j+1];
+                                        nnum[j+1] = alinear;
+                            }
+                    }
+            }
+          //Se imprimen los numeros de forma ordenada 
+         System.out.println();
+          System.out.println("NUMEROS ORDENADOS  ");
+                for (int g = 0; g < n; g++) {
+                       System.out.print( nnum[g]+ ", ");
+        }
+    }
 }
